3a6daabb838b977a279102fc4e804e98daf0831a
Sunny Manik <sundeepkmanik@gmail.com>
2018-10-14 23:34:17 -0700

    set up firebase on client

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+import keys from './keys';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +12,19 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// check keys
+console.log(keys.firebase);
+
+// initialize firebase
+firebase.initializeApp(keys.firebase);
+
+const messaging = firebase.messaging();
+
+messaging.requestPermission()
+.then(function() {
+	console.log('Have permission.');
+})
+.catch(function(err) {
+	console.log('Error occurred.');
+});
