a4621a663dcab3eb8c1655e73cc6669fdc99ca37
Sunny Manik <sundeepkmanik@gmail.com>
2018-10-14 23:58:06 -0700

    firebase token

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import firebase from 'firebase';
 import keys from './keys';
 
@@ -11,7 +10,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
 
 // check keys
 console.log(keys.firebase);
@@ -21,10 +19,23 @@ firebase.initializeApp(keys.firebase);
 
 const messaging = firebase.messaging();
 
+// navigator.serviceWorker
+// 	.register('./firebase-messaging-sw.js')
+// 	.then((registration) => {
+// 		firebase.messaging().useServiceWorker(registration);
+// 	})
+// 	.catch(function(err) {
+// 		console.log('Error sw' + err);
+// 	});
+
 messaging.requestPermission()
 .then(function() {
 	console.log('Have permission.');
+	return messaging.getToken();
+})
+.then(function(token) {
+	console.log('This is token: ', token);
 })
 .catch(function(err) {
-	console.log('Error occurred.');
+	console.log('Error occurred' + err);
 });
